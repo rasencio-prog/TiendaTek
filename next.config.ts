0a72ca31bf4402ceec1595e51a8c5d4aa29bd0d4
rasencio-prog <rasencio@azurian.com>
2025-07-20 23:29:55 -0400

    correccion despliegue raiz

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+export default nextConfig
